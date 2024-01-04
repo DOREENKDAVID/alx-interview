eec89679ae3288ce5f56a746c6cd195fc1498347
DOREENKDAVID <doreenkinya153@gmail.com>
2024-01-04 06:20:16 -0800

    utf8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,22 +14,23 @@ def validUTF8(data: List[int]) -> bool:
         (interpreted as an integer) that indicate
         the start of a character in a UTF-8 sequence."""
 
-        mask = 1 <<(8 -1) #10000000
+        mask = 1 << (8 - 1)
         i = 0
 
-        while num & mask: #197 11000110 $ 10000000
+        while num & mask:
             mask >>=1
-            i +=1
+            i += 1
         return i
     i = 0
     while i < len(data):
         j = check(data[i])
-        k = i + j -(j != 0)
-        i +=1
-        if  j == 1 or j > 4 or k >=len(data):
+        k = i + j - (j != 0)
+        i += 1
+        if j == 1 or j > 4 or k >= len(data):
             return False
-        while i < len(data) and i <=k:
+        while i < len(data) and i <= k:
             current = check(data[i])
-            if current !=1:return False
-            i +=1
+            if current != 1:
+                return False
+            i += 1
     return True
