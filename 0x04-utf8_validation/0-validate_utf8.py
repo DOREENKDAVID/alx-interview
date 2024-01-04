0fabf886e0743d24b068f35ee714fb9ca352a4f8
DOREENKDAVID <doreenkinya153@gmail.com>
2024-01-04 06:00:46 -0800

    method that determines if a given data set represents a valid UTF-8 encoding.

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""unicode transformation format"""
+
+
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """takes a list of integers (representing bytes) and
+    checks if they form a valid UTF-8 sequence."""
+
+    def check(num):
+        """function determines the number of leading bits in a byte
+        (interpreted as an integer) that indicate
+        the start of a character in a UTF-8 sequence."""
+
+        mask = 1 <<(8 -1) #10000000
+        i = 0
+
+        while num & mask: #197 11000110 $ 10000000
+            mask >>=1
+            i +=1
+        return i
+    i = 0
+    while i < len(data):
+        j = check(data[i])
+        k = i + j -(j != 0)
+        i +=1
+        if  j == 1 or j > 4 or k >=len(data):
+            return False
+        while i < len(data) and i <=k:
+            current = check(data[i])
+            if current !=1:return False
+            i +=1
+    return True
