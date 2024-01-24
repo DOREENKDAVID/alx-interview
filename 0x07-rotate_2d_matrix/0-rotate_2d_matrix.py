598b1f6b8c6f95b11bbdec4f016e2ec5d6bc78e2
DOREENKDAVID <doreenkinya153@gmail.com>
2024-01-24 14:50:10 -0800

    Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+from typing import List
+
+
+def rotate_2d_matrix(matrix):
+    """rotate a n * n matrix rows become columns
+    Do not return anything  modify in place"""
+
+    left, right = 0, len(matrix) - 1
+    while left < right:
+        for i in range(right - left):
+            # pointers
+            top, bottom = left, right
+
+            # save the topleft element in temp variable
+            topleft = matrix[top][left + i]
+
+            # move botom left element to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
+
+            # move bottom right element to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+
+            # move top right element to bottop right position [n,n]
+            matrix[bottom][right - i] = matrix[top + i][right]
+
+            # move the saved topleft element in top right position
+            matrix[top + i][right] = topleft
+
+        left += 1
+        right -= 1
